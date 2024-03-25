763544e9471127869521d33a4e6f6b6b52e3fa4c
basniniv <155753031+basniniv@users.noreply.github.com>
2024-03-25 16:58:08 +0400

    task 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         userDao.removeUserById(1);
         userDao.getAllUsers();
-//        userDao.cleanUsersTable();
-//        userDao.dropUsersTable();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
 }
