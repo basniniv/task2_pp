0e9650e5fbdb3e045f6cb76db86ad1c4f5641a09
basniniv <155753031+basniniv@users.noreply.github.com>
2024-03-25 16:40:14 +0400

    task 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,90 @@
 package jm.task.core.jdbc.dao;
 
+import com.sun.istack.logging.Logger;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
+import jm.task.core.jdbc.util.Util;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+  private Connection connection = Util.getConnection();
+  private  final Logger logger = Logger.getLogger(UserDaoJDBCImpl.class.getName().getClass());
 
+  public UserDaoJDBCImpl() {}
+
+  public void createUsersTable() {
+    try (Statement statement = connection.createStatement()) {
+      statement.execute(
+          "CREATE TABLE IF NOT EXISTS users"
+              + "(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, "
+              + "name VARCHAR(45) NOT NULL, "
+              + "lastName VARCHAR(45) NOT NULL,"
+              + "age TINYINT NOT NULL)");
+    } catch (SQLException e) {
+      logger.log(Level.INFO,"При создании таблицы произошла ошибка");
     }
+  }
 
-    public void createUsersTable() {
-
+  public void dropUsersTable() {
+    try (Statement statement = connection.createStatement()) {
+      statement.execute("DROP TABLE IF EXISTS users");
+    } catch (SQLException e) {
+      logger.log(Level.INFO,"При удалении пользователей произошла ошибка");
     }
+  }
 
-    public void dropUsersTable() {
-
+  public void saveUser(String name, String lastName, byte age) {
+    try (PreparedStatement preparedStatement =
+        connection.prepareStatement("INSERT INTO users (NAME, LASTNAME, AGE) VALUES (?, ?, ?)")) {
+      preparedStatement.setString(1, name);
+      preparedStatement.setString(2, lastName);
+      preparedStatement.setByte(3, age);
+      preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+      logger.log(Level.INFO,"При создании пользователя произошла ошибка");
     }
+  }
 
-    public void saveUser(String name, String lastName, byte age) {
-
+  public void removeUserById(long id) {
+    try (Statement statement = connection.createStatement()) {
+      statement.execute("DELETE FROM users WHERE ID = id");
+    } catch (SQLException e) {
+      logger.log(Level.INFO,"При удалении пользователя по id произошла ошибка");
     }
+  }
 
-    public void removeUserById(long id) {
+  public List<User> getAllUsers() {
+    List<User> userList = new ArrayList<>();
+    try (Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+      while (resultSet.next()) {
+        User user = new User();
+        user.setId(resultSet.getLong("ID"));
+        user.setName(resultSet.getString("NAME"));
+        user.setLastName(resultSet.getString("LASTNAME"));
+        user.setAge(resultSet.getByte("AGE"));
 
+        userList.add(user);
+      }
+    } catch (SQLException e) {
+      logger.log(Level.INFO,"При запросе данных из  таблицы произошла ошибка");
     }
+    userList.forEach(System.out::println);
+    return userList;
+  }
 
-    public List<User> getAllUsers() {
-        return null;
+  public void cleanUsersTable() {
+    try (Statement statement = connection.createStatement()) {
+      statement.execute("DELETE FROM users");
+    } catch (SQLException e) {
+      logger.log(Level.INFO,"При очистке таблицы произошла ошибка");
     }
-
-    public void cleanUsersTable() {
-
-    }
+  }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import com.sun.istack.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.util.logging.Level;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static final Logger logger = Logger.getLogger(UserServiceImpl.class.getName().getClass());
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            logger.log(Level.INFO,"Не удалось загрузить класс драйвера");
+        }
+        return connection;
+    }
+
 }
